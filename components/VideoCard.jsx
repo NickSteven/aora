bc00411cdd9c22c04fa63fa0c8971b1f5dd388d7
Nick Steven <nickandoniaina@gmail.com>
2025-01-21 21:26:58 +0300

    [FEAT] (home) implement save video functionnality

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -3,11 +3,12 @@ import { icons } from '../constants'
 import{ React, useState} from 'react'
 import { likeVideo } from '../lib/appwrite';
 import { useVideoPlayer,VideoView } from 'expo-video';
-import { Image, StyleSheet, Text, TouchableOpacity, View, Alert } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View, Alert, Pressable, Modal } from 'react-native'
 
 
 const VideoCard = ({video:{$id, title,thumbnail, video, creator:{username, avatar}}, userId}) => {
     const [play, setPlay] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false)
     const player = useVideoPlayer(video, player => {
         player.loop = false;
         // player.play(); // play the video automatically when component mount
@@ -18,18 +19,16 @@ const VideoCard = ({video:{$id, title,thumbnail, video, creator:{username, avata
       try {
         await likeVideo(videoId, userId);
         Alert.alert("Success","Post saved successfully")
+        setModalVisible(!modalVisible)
       } catch (error) {
         Alert.alert("Error,", error.message)
       }
+      console.log("Hello")
     }
       
   return (
     <View className="flex-col items-center px-4 mb-14">
-  
-
-     
         <View className="flex-row gap-3 items-start">
-          <TouchableOpacity className="w-full" onPress={() => saveVideo($id, userId)}>
             <View className="justify-center items-center flex-row flex-1">
                 <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
                     {/**Put uri property when it's a remote an url image) */}
@@ -43,9 +42,11 @@ const VideoCard = ({video:{$id, title,thumbnail, video, creator:{username, avata
                     <Text className="text-xs text-gray-100 font-pregular">{username}</Text>
                 </View>
             </View>
-            </TouchableOpacity>
             <View className="pt-2">
+              <Pressable className="w-full " onPress={() => setModalVisible(!modalVisible)} hitSlop={20}>
                 <Image source={icons.menu} className="w-5 h-5" resizeMode="contain"/>
+              </Pressable>
+
             </View>
         </View>
         {
@@ -79,6 +80,27 @@ const VideoCard = ({video:{$id, title,thumbnail, video, creator:{username, avata
                 </TouchableOpacity>
             )
         }
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+        >
+            <View className="flex flex-col bg-white justify-center items-center  mt-[300px] mx-24 rounded-lg">
+              <View className="w-full flex-row justify-between px-3">
+              <Text className="text-primary font-psemibold"></Text>
+                <Text className="text-primary font-psemibold">Menu</Text>
+                <Pressable onPress={() => setModalVisible(!modalVisible)} hitSlop={20}>
+                <Text className="text-primary font-psemibold">X</Text>
+                </Pressable>
+              </View>
+            <View className="border-t-[0.5px] w-full !mx-3"></View>
+            <View className="p-3">
+                <Pressable onPress={() => saveVideo($id, userId)} hitSlop={20}>
+                  <Text className="text-white mb-3 bg-primary p-3 rounded-full">Save video</Text>
+                </Pressable>
+            </View>
+            </View>
+        </Modal>
     </View>
   )
 }
